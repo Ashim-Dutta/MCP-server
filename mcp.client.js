08afb28145ab3134087ac451526b1f12cba706c9
Ashim Dutta <ashimdutta800@gmail.com>
2026-01-16 17:36:12 +0530

    client and server bith are connected

---
+++ mcp.client.js
@@ -0,0 +1,21 @@
+import { Client } from "@modelcontextprotocol/sdk/client";
+import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
+
+
+
+const transport = new StdioClientTransport({
+    command:"node",
+    args:["./mcp.server.js"]
+});
+
+
+const client = new Client({
+    name: "example-client",
+    version:"1.0.0"
+})
+
+await client.connect(transport); 
+
+client.listTools().then(response => {
+    console.log(response)
+})
